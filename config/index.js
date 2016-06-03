974d7e7652c1bf0ee716a295064c3aa804793187
fundon <cfddream@gmail.com>
2016-06-03 10:24:46 +0800

    vue admin

---
+++ config/index.js
@@ -0,0 +1,18 @@
+// see http://vuejs-templates.github.io/webpack for documentation.
+var path = require('path')
+
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: true
+  },
+  dev: {
+    env: require('./dev.env'),
+    port: 8080,
+    proxyTable: {}
+  }
+}
