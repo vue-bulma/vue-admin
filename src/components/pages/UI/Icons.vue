41185ed164163eed12d4591498c70ea7c23f7513
fundon <cfddream@gmail.com>
2016-06-06 21:53:50 +0800

    pages: add UI Icons

--- src/components/pages/UI/Icons.vue
+++ src/components/pages/UI/Icons.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="tile is-ancestor">
     <div class="tile is-parent">
-      <article class="tile is-child box">
+      <article class="tile is-child is-4 box">
         <h1 class="title">Font Awesome</h1>
+        <h3 class="subtitle">
+          <a href="http://fontawesome.io/examples/">More Examples</a>
+        </h3>
         <div class="content">
-          <i class="fa fa-camera-retro"></i> normal
-          <i class="fa fa-camera-retro fa-lg"></i> fa-lg
-          <i class="fa fa-camera-retro fa-2x"></i> fa-2x
-          <i class="fa fa-camera-retro fa-3x"></i> fa-3x
-          <i class="fa fa-camera-retro fa-4x"></i> fa-4x
-          <i class="fa fa-camera-retro fa-5x"></i> fa-5x
+          <p>
+            <i class="fa fa-camera-retro"></i>
+          </p>
+          <p>
+            <i class="fa fa-camera-retro fa-lg"></i> fa-lg
+          </p>
+          <p>
+            <i class="fa fa-camera-retro fa-2x"></i> fa-2x
+          </p>
+          <p>
+            <i class="fa fa-camera-retro fa-3x"></i> fa-3x
+          </p>
+          <p>
+            <i class="fa fa-camera-retro fa-4x"></i> fa-4x
+          </p>
+          <p>
+            <i class="fa fa-camera-retro fa-5x"></i> fa-5x
+          </p>
         </div>
       </article>
     </div>
