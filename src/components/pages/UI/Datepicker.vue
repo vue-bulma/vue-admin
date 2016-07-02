55a7600975f51e407411c3d709f13d13a2b4081d
fundon <cfddream@gmail.com>
2016-07-02 15:29:00 +0800

    Datepicker: init

---
+++ src/components/pages/UI/Datepicker.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <article class="tile is-child box">
+          <h1 class="title"></h1>
+          <div class="block is-flex">
+            <datepicker></datepicker>
+          </div>
+        </article>
+      </div>
+
+      <div class="tile is-parent">
+        <article class="tile is-child box">
+          <h1 class="title"></h1>
+          <div class="block is-flex">
+          </div>
+        </article>
+      </div>
+
+      <div class="tile is-parent">
+        <article class="tile is-child box">
+          <h1 class="title"></h1>
+          <div class="block is-flex">
+          </div>
+        </article>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Datepicker from '../../ui/Datepicker'
+
+export default {
+  components: {
+    Datepicker
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.column {
+  margin: 10px;
+  border: 1px solid #fce473;
+}
+</style>
---
+++ src/components/ui/Datepicker.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="control">
+    <input v-el:input class="input" type="text" placeholder="Pick date" />
+  </p>
+</template>
+
+<script>
+import flatpickr from 'flatpickr'
+
+export default {
+
+  ready () {
+    this.datepicker = flatpickr(this.$els.input, {})
+  }
+
+}
+</script>
+
+<style>
+@import '~flatpickr/dist/flatpickr.min.css';
+</style>
