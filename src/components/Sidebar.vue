cf71ea6c7cd4a55a1e77986aac431c30184115a4
Fangdun Cai (Fundon) cfddream@gmail.com <cfddream@gmail.com>
2016-07-15 21:48:53 +0800

    Sidebar: use touchable instead of is-unselectable

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,7 +2,7 @@
   <aside class="menu app-sidebar animated" :class="{ 'slideInLeft': sidebar.opened, 'slideOutLeft': !sidebar.opened }">
     <ul class="menu-list">
       <li v-for="item in menu">
-        <a class="is-unselectable" v-link="{ name: item.link }" @click="toggle(item, $event)" :aria-expanded="isExpanded(item) ? 'true' : 'false'">
+        <a class="touchable" v-link="{ name: item.link }" @click="toggle(item, $event)" :aria-expanded="isExpanded(item) ? 'true' : 'false'">
           <span class="icon is-small" v-if="item.icon">
             <i :class="['fa', item.icon]"></i>
           </span>
@@ -13,7 +13,7 @@
         </a>
         <ul v-show="isReady && item.expanded" :class="{ 'collapse': item.subMenu }" @click="autoClose" transition="menu-expand">
           <li v-for="subItem in item.subMenu">
-            <a class="is-unselectable" v-link="{ name: subItem.link }">{{ subItem.label }}</a>
+            <a class="touchable" v-link="{ name: subItem.link }">{{ subItem.label }}</a>
           </li>
         </ul>
       </li>
