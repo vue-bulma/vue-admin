6f96058bcfe7a17d811b922e7bd765f5e18c2806
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-08-10 18:39:04 +0800

    App: add Sidebar

--- client/App.vue
+++ client/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <navbar></navbar>
+    <sidebar></sidebar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import Navbar from './components/layout/Navbar'
+import Sidebar from './components/layout/Sidebar'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Sidebar
   }
 }
 </script>
---
+++ client/components/layout/Sidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <aside class="menu app-sidebar">
+    <ul class="menu-list">
+      <li v-for="item in menu">
+        <a class="touchable" v-link="{ name: item.link }" @click="toggle(item, $event)" :aria-expanded="isExpanded(item) ? 'true' : 'false'">
+          <span class="icon is-small" v-if="item.icon">
+            <i :class="['fa', item.icon]"></i>
+          </span>
+          <span>{{ item.label }}</span>
+          <span class="icon is-small is-angle" v-if="item.subMenu">
+            <i class="fa fa-angle-down"></i>
+          </span>
+        </a>
+        <ul v-show="isReady && item.expanded" :class="{ 'collapse': item.subMenu }" @click="autoClose" transition="menu-expand">
+          <li v-for="subItem in item.subMenu">
+            <a class="touchable" v-link="{ name: subItem.link }">{{ subItem.label }}</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      menu: []
+    }
+  }
+
+}
+</script>
