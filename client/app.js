a89ab418d4df3e18e6aad908b4caf10d6fb9d25a
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-08-11 02:36:09 +0800

    App: add filters

--- client/app.js
+++ client/app.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
-import {sync} from 'vuex-router-sync'
+import { sync } from 'vuex-router-sync'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as filters from './filters'
+
 sync(store, router)
+
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
 
 const app = new Vue({
   router,
--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
-import {app} from './app'
+import { app } from './app'
 
 app.$mount('#app')
