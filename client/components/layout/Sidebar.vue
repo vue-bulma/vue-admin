2b65289af4e8180938d9910a9a5ae107f4670e67
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-08-14 23:54:15 +0800

    Sidebar: add style

--- client/components/layout/Sidebar.vue
+++ client/components/layout/Sidebar.vue
@@ -1,8 +1,56 @@
 <template>
   <aside class="menu app-sidebar">
     <p class="menu-label">
-      Components
+      General
     </p>
+    <ul class="menu-list">
+      <li>
+        <a href="#">
+          <span class="icon is-small"><i class="fa fa-tachometer"></i></span>
+          Dashboard
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <span class="icon is-small"><i class="fa fa-bar-chart-o"></i></span>
+          Charts
+          <span class="icon is-small is-angle"><i class="fa fa-angle-down"></i></span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <span class="icon is-small"><i class="fa fa-building-o"></i></span>
+          Components
+          <span class="icon is-small is-angle"><i class="fa fa-angle-down"></i></span>
+        </a>
+        <ul>
+          <li><a href="#">BackToTop</a></li>
+          <li><a href="#">Collapse</a></li>
+          <li><a href="#">Datepicker</a></li>
+          <li><a href="#">Emoji</a></li>
+          <li><a href="#">Message</a></li>
+          <li><a href="#">Modal</a></li>
+          <li><a href="#">Notification</a></li>
+          <li><a href="#">Progress</a></li>
+          <li><a href="#">Rating</a></li>
+          <li><a href="#">Slider</a></li>
+          <li><a href="#">Switch</a></li>
+          <li><a href="#">Tabs</a></li>
+          <li><a href="#">Tooltip</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <span class="icon is-small"><i class="fa fa-table"></i></span>
+          Tables
+          <span class="icon is-small is-angle"><i class="fa fa-angle-down"></i></span>
+        </a>
+        <ul>
+          <li><a href="#">Basic</a></li>
+          <li><a href="#">Handsontable</a></li>
+        </ul>
+      </li>
+    </ul>
   </aside>
 </template>
 
@@ -27,7 +75,7 @@ export default {
   top: 50px;
   left: 0;
   bottom: 0;
-  padding: 20px 10px 50px;
+  padding: 20px 0 50px;
   width: 180px;
   min-width: 45px;
   max-height: 100vh;
@@ -42,9 +90,20 @@ export default {
     transform: translate3d(-180px, 0, 0);
   }
 
+  .icon {
+    vertical-align: baseline;
+    &.is-angle {
+      position: absolute;
+      right: 10px;
+    }
+  }
+
   .menu-label {
-    cursor: pointer;
+    padding-left: 5px;
+  }
+
+  .menu-list li ul {
+    margin-left: 15px;
   }
 }
-
 </style>
