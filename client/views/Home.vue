7cefaeb7c76dd0b5d17471df49d66020a3d78150
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-09-24 15:05:21 +0800

    views: update home

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="page">
+  <div class="content has-text-centered">
     <p>
-      Upgrating to Vue 2.0!
+      <img width="200" src="~assets/logo.svg" alt="Vue Admin Panel Framework">
     </p>
+
+    <h1 class="is-title is-bold">Vue Admin</h1>
+
+    <p>
+      <strong>Vue Admin Panel Framework</strong>,
+      <a href="https://vue-admin.fundon.me">Live Demo</a>
+    </p>
+
+    <p>Supports Vue 2.0!</p>
   </div>
 </template>
-
-<script>
-export default {
-}
-</script>
