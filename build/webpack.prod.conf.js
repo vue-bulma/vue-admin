3be5eed0980d7b05a9bc79860344bdfa03d1a2c5
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-09-25 18:13:07 +0800

    webpack: should set title

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -44,6 +44,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
+      title: 'Vue Admin',
       filename: process.env.NODE_ENV === 'testing'
         ? 'index.html'
         : config.build.index,
