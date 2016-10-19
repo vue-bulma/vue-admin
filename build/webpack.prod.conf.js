353dcda4233e905b22912b3259aeb9d1b95644bc
Fangdun Cai <cfddream@gmail.com>
2016-10-20 03:12:45 +0800

    webpack: fix the public path for loading fonts in electron

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -20,7 +20,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
-    publicPath: '',
+    publicPath: path.join(__dirname, '../dist/'),
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
