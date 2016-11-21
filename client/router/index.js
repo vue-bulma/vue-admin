76fb45113a6d6bbadd515deec4ed6106a5e1757b
Ivan Novikov <novikov@amiforus.com>
2016-11-21 14:37:32 +0300

    Reverted back vuex-store alias

--- client/router/index.js
+++ client/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import menuModule from '../store/modules/menu'
+import menuModule from 'vuex-store/modules/menu'
 Vue.use(Router)
 
 export default new Router({
