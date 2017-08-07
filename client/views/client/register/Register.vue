70ca8f2f35a67c06aadbb2f8b31285e8f4c63e92
acampagnaro <alexandre.campagnaro@gmail.com>
2017-08-06 22:18:23 -0300

    Adicionando o create do user

--- client/views/client/register/Register.vue
+++ client/views/client/register/Register.vue
@@ -7,28 +7,46 @@
         <div class="block">
 
           <label class="label">Nome</label>
-          <p class="control">
-            <input class="input" type="text" placeholder="Text input">
-          </p>
-          <label class="label">CRM</label>
-          <p class="control">
-            <input class="input" type="text" placeholder="Text input">
-          </p>
-          <label class="label">Email</label>
           <p class="control has-icon has-icon-right">
-            <input class="input is-danger" type="text" placeholder="Email input" value="hello@">
+            <input class="input is-danger" type="text" placeholder="Nome" v-model="data.user.name">
             <span class="icon is-small">
               <i class="fa fa-warning"></i>
             </span>
-            <span class="help is-danger">This email is invalid</span>
+            <span class="help is-danger">Nome inválido!</span>
           </p>
+
+          <label class="label">CRM</label>
+          <p class="control has-icon has-icon-right">
+            <input class="input is-danger" type="text" placeholder="CRM" v-model="data.user.crm">
+            <span class="icon is-small">
+              <i class="fa fa-warning"></i>
+            </span>
+            <span class="help is-danger">CRM inválido!</span>
+          </p>
+
+          <label class="label">Email</label>
+          <p class="control has-icon has-icon-right">
+            <input class="input is-danger" type="email" placeholder="Email" v-model="data.user.email">
+            <span class="icon is-small">
+              <i class="fa fa-warning"></i>
+            </span>
+            <span :class="{ hasError: 'help is-danger' }"></span>
+            {{hasError}}
+          </p>
+
           <label class="label">Senha</label>
           <p class="control has-icon has-icon-right">
-            <input class="input is-success" type="password" placeholder="Text input" value="bulma">
+            <input class="input is-danger" type="password" placeholder="Senha" v-model="data.user.password">
+            <span class="help is-danger">Senha inválida!</span>
+            <span class="icon is-small">
+              <i class="fa fa-warning"></i>
+            </span>
+            <!--
+            <input class="input is-success" type="password" placeholder="Senha" v-model="data.user.password">
             <span class="icon is-small">
               <i class="fa fa-check"></i>
             </span>
-            <span class="help is-success">This username is available</span>
+            <span class="help is-success">Senha inválida!</span>-->
           </p>
 
           <p class="control">
@@ -38,7 +56,7 @@
             </label>
           </p>
           <p class="control">
-            <button class="button is-primary">Cadastrar</button>
+            <button class="button is-primary" @click="register()">Cadastrar</button>
             <router-link to="/login">
               <button class="button is-link">Cancelar</button>
             </router-link>
@@ -52,19 +70,48 @@
 
 <script>
 export default {
+  name: 'Register',
+  methods: {
+    validate () {
+      if (this.data.user.name === undefined) {
+        return false
+      }
+    },
+    register () {
+      if (this.validate() === false) {
+        return
+      }
+      this.$http({
+        method: 'post',
+        url: 'http://localhost:8091/users/create',
+        transformResponse: [(data) => {
+          return JSON.parse(data.replace(/T00:00:00/g, ''))
+        }]
+      }).then((response) => {
+        console.log(response)
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+  },
   data () {
     return {
       data: {
-        body: {
-          username: null,
-          password: null
-        },
-        rememberMe: false
+        user: {
+          email: null,
+          password: null,
+          name: null,
+          crm: null
+        }
       },
-      error: null
+      isValidEmail: false,
+      error: null,
+      hasError: true,
+      isActive: true
     }
   }
 }
+
 </script>
 
 <style lang="scss">
